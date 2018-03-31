b79c93f65bd438e214b0963da0b704343d38490a
Javier Mateo <javi.mateosegura@gmail.com>
2018-03-31 13:39:47 +0200

    Implement two pane navigation

--- app/src/main/java/com/example/evilj/bakingapp/BakeryMain.java
+++ app/src/main/java/com/example/evilj/bakingapp/BakeryMain.java
@@ -1,11 +1,17 @@
 package com.example.evilj.bakingapp;
 
+
 import android.content.Intent;
+import android.content.pm.ActivityInfo;
 import android.os.Bundle;
+import android.os.Handler;
+import android.support.annotation.Nullable;
+import android.support.v4.app.FragmentManager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.DividerItemDecoration;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import com.example.evilj.bakingapp.models.Ingredients;
@@ -15,14 +21,18 @@ import com.example.evilj.bakingapp.utils.JSONUtils;
 import butterknife.BindView;
 import butterknife.ButterKnife;
 import butterknife.OnClick;
+import butterknife.Optional;
 
-public class BakeryMain extends AppCompatActivity implements StepsAdapter.StepsCallback {
+public class BakeryMain extends AppCompatActivity implements StepsAdapter.StepsCallback, StepFragment.StepListener {
 
     public static final String KEY_INGREDIENTS = "ingredients";
     @BindView(R.id.bakery_title)
     TextView mIngredientsTv;
     @BindView(R.id.bakery_main_recycler_view)
     RecyclerView mRecyclerView;
+    @Nullable
+    @BindView(R.id.master_flow_linear)
+    LinearLayout mLinearLayout;
 
     public static final String STEP_EXTRA_KEY = "steps";
     public static final String STEP_POSTION_EXTRA_KEY = "pos";
@@ -34,6 +44,10 @@ public class BakeryMain extends AppCompatActivity implements StepsAdapter.StepsC
 
     private Ingredients[] mIngredients;
     private Steps[] mSteps;
+    private boolean mTwoPane;
+    private FragmentManager mFragmentManager;
+    private StepFragment mStepFragment;
+    private int currentPos;
 
 
     @Override
@@ -42,39 +56,55 @@ public class BakeryMain extends AppCompatActivity implements StepsAdapter.StepsC
         setContentView(R.layout.activity_bakery_main);
         ButterKnife.bind(this);
         Intent started = getIntent();
-        if (started==null) throw new RuntimeException();
+        if (started == null) throw new RuntimeException();
         String json = started.getStringExtra(MainActivity.BAKERY_INFORMATION_KEY);
-        if (json==null) throw new RuntimeException();
+        if (json == null) throw new RuntimeException();
+        mTwoPane = mLinearLayout != null;
         mIngredients = JSONUtils.parseIngredients(json);
         mSteps = JSONUtils.parseSteps(json);
+
         mIngredientsTv.setText(R.string.ingredients);
-        mStepsAdapter = new StepsAdapter(mSteps,this,this);
+
+        mStepsAdapter = new StepsAdapter(mSteps, this, this);
         mLinearLayoutManager = new LinearLayoutManager(this);
         mRecyclerView.setAdapter(mStepsAdapter);
         mRecyclerView.setLayoutManager(mLinearLayoutManager);
-        mRecyclerView.addItemDecoration(new DividerItemDecoration(this,mLinearLayoutManager.getOrientation()));
+        mRecyclerView.addItemDecoration(new DividerItemDecoration(this, mLinearLayoutManager.getOrientation()));
+        if (mTwoPane) {
+            mFragmentManager = getSupportFragmentManager();
+            mStepFragment = new StepFragment();
+            mStepFragment.setSteps(mSteps[0]);
+            currentPos = 0;
+            mFragmentManager.beginTransaction().add(R.id.master_flow_frame_layout, mStepFragment).commit();
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_LANDSCAPE);
+        }
     }
 
 
+    @Optional
     @OnClick(R.id.ingredients_layout)
-    void seeIngredients(){
-        Intent intent = new Intent(this,IngredientsActivity.class);
-        intent.putExtra(KEY_INGREDIENTS,mIngredients);
+    void seeIngredients() {
+        Intent intent = new Intent(this, IngredientsActivity.class);
+        intent.putExtra(KEY_INGREDIENTS, mIngredients);
         startActivity(intent);
     }
 
     @Override
     public void onClick(int position) {
-        Intent intent = new Intent(this,StepsActivity.class);
-        intent.putExtra(STEP_EXTRA_KEY,mSteps);
-        intent.putExtra(STEP_POSTION_EXTRA_KEY,position);
-        startActivity(intent);
+        if (mTwoPane) {
+            changeFragment(position);
+        } else {
+            Intent intent = new Intent(this, StepsActivity.class);
+            intent.putExtra(STEP_EXTRA_KEY, mSteps);
+            intent.putExtra(STEP_POSTION_EXTRA_KEY, position);
+            startActivity(intent);
+        }
     }
 
     @Override
     protected void onSaveInstanceState(Bundle outState) {
         super.onSaveInstanceState(outState);
-        outState.putParcelable(RESTORE_RECYCLER_VIEW,mLinearLayoutManager.onSaveInstanceState());
+        outState.putParcelable(RESTORE_RECYCLER_VIEW, mLinearLayoutManager.onSaveInstanceState());
     }
 
     @Override
@@ -82,4 +112,33 @@ public class BakeryMain extends AppCompatActivity implements StepsAdapter.StepsC
         super.onRestoreInstanceState(savedInstanceState);
         mLinearLayoutManager.onRestoreInstanceState(savedInstanceState.getParcelable(RESTORE_RECYCLER_VIEW));
     }
+
+    @Override
+    public void videoCompleted() {
+        if (currentPos != mSteps.length - 1) {
+            Handler handler = new Handler();
+            handler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    nextStepFragment();
+                }
+            }, 3000);//Wait 3 seconds before going into the next Step
+        }
+    }
+
+
+    private void nextStepFragment() {
+        currentPos++;
+        mStepFragment = new StepFragment();
+        mStepFragment.setSteps(mSteps[currentPos]);
+        mFragmentManager.beginTransaction().replace(R.id.master_flow_frame_layout, mStepFragment).commit();
+    }
+
+    private void changeFragment(int position) {
+        currentPos = position;
+        mStepFragment = new StepFragment();
+        mStepFragment.setSteps(mSteps[currentPos]);
+        mFragmentManager.beginTransaction().replace(R.id.master_flow_frame_layout, mStepFragment).commit();
+    }
+
 }
